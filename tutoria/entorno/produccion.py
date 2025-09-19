b53392544f26c27be22d688d5f2ffcc9ec89d2a0
Raul David <raul.david@amentum.net>
2025-09-19 14:02:25 -0600

    add environtment production

--- tutoria/entorno/produccion.py
+++ tutoria/entorno/produccion.py
@@ -1,1 +1,23 @@
 from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['tutorito.pythonanywhere.com']
+
+database = 'tutorito$tutoria'
+user = 'tutorito'
+password = 'UdeG.2025'
+host = 'tutorito.mysql.pythonanywhere-services.com'
+port = '3306'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': database,
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': host,  
+        'PORT': port,  
+    },
+}
+
